8bd2f38a288b22641da100f66f3c86b8b7659598
xingfeng <wangli921016@gmail.com>
2016-05-01 13:42:16 +0800

    完成首页界面的业务逻辑、优化美化还待完善

--- app/src/main/java/importnew/importnewclient/ui/HomePageFragment.java
+++ app/src/main/java/importnew/importnewclient/ui/HomePageFragment.java
@@ -3,25 +3,29 @@ package importnew.importnewclient.ui;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import importnew.importnewclient.R;
 import importnew.importnewclient.adapter.ArticleBlockAdapter;
 import importnew.importnewclient.bean.ArticleBlock;
 import importnew.importnewclient.parser.HomePagerParser;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 
 /**
@@ -36,6 +40,24 @@ public class HomePageFragment extends Fragment {
 
     private List<ArticleBlock> articles;
     private ArticleBlockAdapter mAdapter;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if (msg.what == 1) {
+
+                mRefreshLayout.setRefreshing(false);
+                mRecyclerView.setVisibility(View.VISIBLE);
+                mAdapter = new ArticleBlockAdapter(mContext, mRecyclerView,articles);
+                mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+                mRecyclerView.setAdapter(mAdapter);
+
+            }
+
+        }
+    };
 
     @Override
     public void onAttach(Context context) {
@@ -66,7 +88,6 @@ public class HomePageFragment extends Fragment {
         mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-
                 mRefreshLayout.setRefreshing(true);
                 getHtmlAndParser();
             }
@@ -75,27 +96,62 @@ public class HomePageFragment extends Fragment {
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.homepage_recycler);
         mRecyclerView.setVisibility(View.GONE);
-
+        getHtmlAndParser();
     }
 
-    private void getHtmlAndParser(){
-
-
-        try {
-            OkHttpClient client=new OkHttpClient();
-            Request request=new Request.Builder().url("http://www.importnew.com").build();
-            Response response=client.newCall(request).execute();
-            articles= HomePagerParser.paserHomePage(response.body().toString());
-
-            
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    @Override
+    public void onPause() {
+        super.onPause();
+        mAdapter.flushCache();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mAdapter.cancelAllTasks();
+    }
+
+    private void getHtmlAndParser() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection conn = null;
+                BufferedReader br = null;
+                try {
+
+                    conn = (HttpURLConnection) new URL("http://www.importnew.com").openConnection();
+                    conn.setReadTimeout(10 * 1000);
+                    conn.setConnectTimeout(5 * 1000);
+                    br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuilder result=new StringBuilder();
+                    String line="";
+                    while((line=br.readLine())!=null){
+                        result.append(line);
+                    }
+
+                    articles = HomePagerParser.paserHomePage(result.toString());
+                    handler.sendEmptyMessage(1);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if(conn!=null)
+                        conn.disconnect();
+
+                    try {
+                        if(br!=null)
+                            br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+
+    }
 
 
 }
